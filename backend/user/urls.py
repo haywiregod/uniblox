fe8987b8d5248648a33b075ab418d4aefe69ccce
vikascaplist <vikas.tiwari@caplist.no>
2025-03-27 23:52:17 +0530

    [add] Signup API route

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
 from .views import UserViewSet
 
-urlpatterns = [path("login/", UserViewSet.as_view({"post": "login"}))]
+urlpatterns = [
+    path("login/", UserViewSet.as_view({"post": "login"})),
+    path("signup/", UserViewSet.as_view({"post": "signup"})),
+]
